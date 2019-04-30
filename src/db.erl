ce6b90979be36ebfe2377ce44133ee41eb1250d1
Emilien Pecoul (Ouarzy) <ouarzy@free.fr>
2019-04-30 23:39:19 +0200

    use filtermap to getEventsByStream

--- src/db.erl
+++ src/db.erl
@@ -1,12 +1,21 @@
 -module(db).
 
--export([new/0]).
--export([appendToStream/3]).
+-export([new/0, appendToStream/3, getEventsByStream/2]).
 
 new() ->
     io:fwrite("creating new db\n"),
     [].
 
 appendToStream(Db, StreamId, Event) -> 
-    NewDb = lists:append(Db, [{StreamId, [Event]}]),
+    NewDb = lists:append(Db, [{StreamId, Event}]),
     NewDb.
+
+getEventsByStream(Db, StreamId) ->
+    EventsByStream = lists:filtermap(
+                       fun({_StreamId, Event}) -> 
+                            if _StreamId == StreamId -> { true, Event };
+                            _StreamId /= StreamId -> false
+                            end
+                       end,
+                       Db),
+    {StreamId, EventsByStream}.
--- test/db_test.erl
+++ test/db_test.erl
@@ -5,9 +5,15 @@
 
 new_test() -> ?assertEqual(db:new(), []).
 
-append_test() -> 
+appendToStream_test() -> 
     ExpectedStreamId =uuid:uuid1(),
-    ?assertEqual([{ExpectedStreamId, [{messageTweeted, "hello"}]}], db:appendToStream(db:new(), ExpectedStreamId, {messageTweeted, "hello"})).
+    ?assertEqual([{ExpectedStreamId, {messageTweeted, "hello"}}], db:appendToStream(db:new(), ExpectedStreamId, {messageTweeted, "hello"})).
 
 
+getEventsByStream_test() -> 
+    ExpectedStreamId =uuid:uuid1(),
+    Db = db:new(),
+    DbWithTweeted = db:appendToStream(Db, ExpectedStreamId, {messageTweeted, "hello"}),
+    DbWithDeleted = db:appendToStream(DbWithTweeted, ExpectedStreamId, {messageDeleted}),
+    ?assertEqual({ExpectedStreamId, [{messageTweeted, "hello"}, {messageDeleted}]}, db:getEventsByStream(DbWithDeleted, ExpectedStreamId)).
 
